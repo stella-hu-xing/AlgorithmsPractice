55ff196238b7d4daefa6844015d28da66349e554
Xing Hu <xinghu0164@gmail.com>
2017-03-07 10:47:09 +1100

    no message

---
+++ src/easy_level/SingleNumber_136.java
@@ -0,0 +1,25 @@
+package easy_level;
+
+/**
+ * 
+ * Given an array of integers, every element appears twice except for one. Find
+ * that single one.
+ * 
+ * Note: Your algorithm should have a linear runtime complexity. Could you
+ * implement it without using extra memory?
+ * 
+ * @author xinghu
+ *
+ */
+public class SingleNumber_136 {
+
+	public static void main(String[] args) {
+
+	}
+
+	public int singleNumber(int[] nums) {
+
+		int answer = 0;
+		return answer;
+	}
+}
