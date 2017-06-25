0e9fc77daf5d63626b36aa7316acbeff3ba6cf10
Xing Hu <xinghu0164@gmail.com>
2017-06-25 12:46:34 +1000

    add "merge trees"_617

---
+++ src/easy_level/TreeNode.java
@@ -0,0 +1,17 @@
+package easy_level;
+
+/**
+ * helper for 617
+ * 
+ * @author xinghu
+ *
+ */
+public class TreeNode {
+	int val;
+	TreeNode left;
+	TreeNode right;
+
+	TreeNode(int x) {
+		val = x;
+	}
+}
