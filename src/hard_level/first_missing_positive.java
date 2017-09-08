c71039417c8f29215176bcc69b3c38fff4ede1c7
Xing Hu <xinghu0164@gmail.com>
2017-09-08 14:57:19 +1000

    add one perfect method

--- src/hard_level/first_missing_positive.java
+++ src/hard_level/first_missing_positive.java
@@ -25,12 +25,13 @@ public class first_missing_positive {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		first_missing_positive cdt = new first_missing_positive();
-		int[] A = {1};
+		int[] A = { 1 };
 		System.out.println("final:" + cdt.solution(A));
 	}
 
 	public int solution(int[] A) {
-		if(A.length==0) return 1;
+		if (A.length == 0)
+			return 1;
 		Arrays.sort(A);
 		List<Integer> list = new ArrayList<Integer>();
 		for (int i = 0; i < A.length; i++) {
@@ -39,12 +40,36 @@ public class first_missing_positive {
 
 			}
 		}
-		if(list.size()==0) return 1;
-		for(int v=0;v<list.size();v++){
-			if(list.get(v)!= v+1){
-				return v+1;
+		if (list.size() == 0)
+			return 1;
+		for (int v = 0; v < list.size(); v++) {
+			if (list.get(v) != v + 1) {
+				return v + 1;
 			}
 		}
-		return list.get(list.size()-1)+1;
+		return list.get(list.size() - 1) + 1;
 	}
+
+	public int solution100(int[] A) {
+		int i = 0;
+		while (i < A.length) {
+			if (A[i] == i + 1 || A[i] <= 0 || A[i] > A.length)
+				i++;
+			else if (A[A[i] - 1] != A[i])
+				swap(A, i, A[i] - 1);
+			else
+				i++;
+		}
+		i = 0;
+		while (i < A.length && A[i] == i + 1)
+			i++;
+		return i + 1;
+	}
+
+	private void swap(int[] A, int i, int j) {
+		int temp = A[i];
+		A[i] = A[j];
+		A[j] = temp;
+	}
+
 }
